8d01d068e3fbae9208a20fafa8fd98685ddeedda
sloniupck <sloniupython@gmail.com>
2022-10-20 19:00:29 +0200

    Added drf-spectacular descriptions to endpoints

--- app/shop/views.py
+++ app/shop/views.py
@@ -1,13 +1,28 @@
-from rest_framework import viewsets, generics
+from rest_framework import viewsets, generics, mixins
 from core.models import Product, Category, CartItem, Cart, Order
-from .serializers import ProductSerializer, CategorySerializer, CartItemSerializer, CartSerializer, OrderSerializer, OrderUpdateSerializer
+from .serializers import (
+    ProductSerializer,
+    CategorySerializer,
+    CartItemSerializer,
+    CartSerializer,
+    OrderSerializer,
+    OrderUpdateSerializer)
 from .permissions import IsStaffOrReadOnly, IsStaff
 from django.shortcuts import get_object_or_404
 from rest_framework import filters
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.permissions import IsAuthenticated
+from drf_spectacular.utils import extend_schema, extend_schema_view
 
 
+@extend_schema(
+    description='Endpoint for products. You can search by name or categories(put ids of categories) and order by '
+                'quantity or price.',
+    methods=["GET"]
+)
+@extend_schema(
+    description='This methods are allowed only for staff users.', methods=["POST", "PUT", "PATCH", "DELETE"]
+)
 class ProductViewSet(viewsets.ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
@@ -20,13 +35,27 @@ class ProductViewSet(viewsets.ModelViewSet):
         return serializer.save(user=self.request.user)
 
 
+@extend_schema(
+    description='Endpoint for categories.',
+    methods=["GET"]
+)
+@extend_schema(
+    description='This methods are allowed only for staff users.', methods=["POST", "PUT", "PATCH", "DELETE"]
+)
 class CategoryViewSet(viewsets.ModelViewSet):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
     permission_classes = [IsStaffOrReadOnly]
 
 
-class CartItemViewSet(viewsets.ModelViewSet):
+class CartItemViewSet(mixins.RetrieveModelMixin,
+                      mixins.UpdateModelMixin,
+                      mixins.CreateModelMixin,
+                      viewsets.GenericViewSet):
+    """
+    Endpoint for adding or editing items to cart.
+    Please type product id and quantity. Cart is auto-deleted when order is created.
+    """
     queryset = CartItem.objects.all()
     serializer_class = CartItemSerializer
     permission_classes = [IsAuthenticated]
@@ -36,6 +65,10 @@ class CartItemViewSet(viewsets.ModelViewSet):
 
 
 class CartApiView(generics.RetrieveDestroyAPIView):
+    """
+    Endpoint for check products in cart.
+    To add products, please use Cart-Item endpoint.
+    """
     serializer_class = CartSerializer
     permission_classes = [IsAuthenticated]
 
@@ -43,6 +76,14 @@ class CartApiView(generics.RetrieveDestroyAPIView):
         return get_object_or_404(Cart, user=self.request.user)
 
 
+@extend_schema_view(
+    list=extend_schema(description="Endpoint for check all user's orders."),
+    retrieve=extend_schema(description='Endpoint for check order detail'),
+    create=extend_schema(description='Endpoint for create order from cart.'),
+    update=extend_schema(description='This method is allowed only for staff users'),
+    partial_update=extend_schema(description='This method is allowed only for staff users'),
+    destroy=extend_schema(description='This method is allowed only for staff users')
+)
 class OrderViewSet(viewsets.ModelViewSet):
     queryset = Order.objects.all()
     serializer_class = OrderSerializer
